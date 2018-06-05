4a921cae4cfaa76a9e42ae4bac3a2a1bdd3f9648
Clavel Julien <julien.clavel@hotmail.fr>
2018-06-05 02:20:57 +0200

    updated algorithm

--- src/glassofast.f90
+++ src/glassofast.f90
@@ -1,11 +1,11 @@
-      subroutine glassofast(n,S,L,thr,maxIt,msg,warm,X,W,Wd,WXj,info)
+      subroutine glassofast(n,S,L,thr,maxIt,msg,warm,X,W,Wd,WXDj,info)
 !
 !     .. Scalar Arguments ..
       implicit double precision(a-h, o-z)
-      integer n, warm, msg, maxIt
+      integer n, warm, msg, maxIt, iter
 !     ..
 !     .. Array Arguments ..
-      double precision S(n,n), L(n,n), X(n,n), W(n,n), Wd(n), WXj(n)
+      double precision S(n,n), L(n,n), X(n,n), W(n,n), Wd(n), WXDj(n)
 !     ..
 !
 !  Purpose
@@ -16,10 +16,10 @@
 !    J. Friedman, T. Hastie, R. Tibshirani:
 !    Sparse inverse covariance estimation with the graphical lasso
 !    Biostatistics, 9(3):432-441, July 2008.
-!  This implementation is documented in the technical report:
+!  This implementation is documented in the paper:
 !    M. A. Sustik B. Calderhead:
-!    GLASSOFAST: An efficient GLASSO implementation
-!    Technical Report TR-12-29, University of Texas at Austin
+!    Efficient Glasso implementation
+!    Journal of Machine Learning Research, submitted.
 !
 !  Arguments
 !  =========
@@ -36,9 +36,8 @@
 !  thr    (input) double precision
 !         Convergence threshold
 !
-!  maxIt  (input/output) integer
-!         Maximum number of whole matrix sweeps on input, actual number
-!         of sweeps on output
+!  maxIt  (input) integer
+!         Maximum number of whole matrix sweeps
 !
 !  msg    (input) integer
 !         Controls amount of messages printed
@@ -52,21 +51,18 @@
 !  W      (input/output) double precision array, dimension n x n
 !         Covariance matrix estimate
 !
-!  Wd     (input) double precision array, dimension n
-!         Internally used workspace
-!
-!  WXj    (input) double precision array, dimension n
-!         Internally used workspace
-!
 !  info   (output) integer
 !         Indicates errors
-!
-!  Wd, WDXj are not allocatable arrays, because gfortran/gdb has trouble
-!  displaying those.
 
-integer iter
+!double precision, dimension (:), allocatable :: Wd, WXDj
 double precision EPS
 parameter (EPS = 1.1e-16)
+!allocate(Wd(1:n), stat = info)
+!allocate(WXDj(1:n), stat = ierr)
+!info = info + ierr
+!if (info .ne. 0) then
+!   return
+!endif
 info = 0
 shr = sum(abs(S))
 do i = 1,n
@@ -86,78 +82,74 @@ if (shr .eq. 0.0) then
    return
 endif
 shr = thr*shr/(n-1)
-thrLasso = shr/n
-if (thrLasso .lt. 2*EPS) then
-   thrLasso = 2*EPS
-end if
 if (warm .eq. 0) then
    W = S
    X = 0.0
+   do i = 1,n
+      X(i,i) = 1.0
+   enddo
 else
    do i = 1,n
-     X(1:n,i) = -X(1:n,i)/X(i,i)
-     X(i,i) = 0
+     tmp = X(i,i)
+     X(1:n,i) = X(1:n,i)/tmp
   end do
-end if
+endif
 do i = 1,n
-   Wd(i) = S(i,i) + L(i,i)
-   W(i,i) = Wd(i)
-end do
+   W(i,i) = S(i,i) + L(i,i)
+   Wd(i) = W(i,i)
+enddo
 do iter = 1,maxIt
-! if (msg .ne. 0) write(6,*) "iteration =", iter
-!   print iterations to the console (ok with CRAN)
-if (msg .ne. 0)  call intpr('iter:',-1,iter,1)
-   dw = 0.0
+!   JC: print iterations to the console (ok with CRAN)
+    if (msg .ne. 0)  call intpr('iter:',-1,iter,1)
+dw = 0.0
    do j = 1,n
-      WXj(1:n) = 0.0
+      WXDj(1:n) = 0.0
 !     We exploit sparsity of X when computing column j of W*X*D:
       do i = 1,n
          if (X(i,j) .ne. 0.0) then
-            WXj = WXj + W(:,i)*X(i,j)
+            WXDj = WXDj - W(:,i)*X(i,j)
          endif
       enddo
       do
          dlx = 0.0
          do i = 1,n
             if (i .ne. j) then
-               a = S(i,j) - WXj(i) + Wd(i)*X(i,j)
-               b = abs(a) - L(i,j)
-               if (b .gt. 0.0) then
-                  c = sign(b, a)/Wd(i)
-               else
-                  c = 0.0
+               diff = S(i,j) - WXDj(i) - W(i,j) - Wd(i)*X(i,j)
+               tmp = abs(diff) - L(i,j)
+               u = -X(i,j)
+               X(i,j) = 0.0
+               if (tmp .gt. 0.0) then
+                  X(i,j) = -sign(tmp,diff)/Wd(i)
+                  u = u + X(i,j)
                endif
-               delta = c - X(i,j)
-               if (delta .ne. 0.0) then
-                  X(i,j) = c
-                  WXj(1:n) = WXj(1:n) + W(:,i)*delta
-                  dlx = max(dlx, abs(delta))
+               if (u .ne. 0.0) then
+                  WXDj(1:n) = WXDj(1:n) - W(:,i)*u
+                  dlx = max(dlx, abs(u))
                endif
             endif
          enddo
-         if (dlx .lt. thrLasso) then
+         if (dlx .lt. thr) then
             exit
          endif
       enddo
-      WXj(j) = Wd(j)
-      dw = max(dw, sum(abs(WXj(1:n) - W(:,j))))
-      W(:,j) = WXj(1:n)
-      W(j,:) = WXj(1:n)
+      dw = max(dw,sum(abs(WXDj(1:n))) - abs(WXDj(j)))
+      WXDj(1:n) = WXDj(1:n) + W(:,j)
+      W(:,j) = WXDj(1:n)
+      W(j,:) = WXDj(1:n)
+      W(j,j) = Wd(j)
    enddo
-!   write(6,*) "  dw =", dw
    if (dw .le. shr) then
       exit
    endif
 enddo
 do i = 1,n
-   tmp = 1/(Wd(i) - sum(X(:,i)*W(:,i)))
-   X(1:n,i) = -tmp*X(1:n,i)
-   X(i,i) = tmp
+   tmp = 1/(sum(X(:,i)*W(:,i)))
+   X(1:n,i) = tmp*X(1:n,i)
 enddo
-do i = 1,n-1
-   X(i+1:n,i) = (X(i+1:n,i) + X(i,i+1:n))/2;
-   X(i,i+1:n) = X(i+1:n,i) 
+do i = 1,n
+   X(1:n,i) = (X(1:n,i) + X(i,1:n))/2;
+   X(i, 1:n) = X(1:n,i) 
 enddo
-maxIt = iter
+maxIt = iter ! JC : Update the number of iterations
 return
 end subroutine glassofast
